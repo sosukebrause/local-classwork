7086545ccaec2bec92cc6fbc2d04e5b001100dce
Sosuke Brause <brosuke89@gmail.com>
2020-04-25 11:20:44 -0700

    day 5 acts

---
+++ 5/day3/app.js
@@ -0,0 +1,21 @@
+var sayHello = require("./sayHello");
+​var sumArray = require("sumArray");
+var message = sayHello("Kenny!");
+console.log(message);
+​
+var sumArray = require("sumArray");
+​
+var num = sumArray([1, 2, 3]);
+​
+console.log(num);
+
+function largestNum(arr){
+for (let i = 0; i < array.length; i++) {
+  if (arr[i] > largestNumber) {
+    largestNumber = arr[i];
+  }
+  console.log(largestNumber);
+  
+}
+}
+largestNum([12,34,56,68]);
